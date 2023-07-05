2ec276d4fd626b35ff82ffb7ea0ec65b159fef13
mmir01 <mmir01@github.com>
2023-07-05 16:42:26 +0100

    Added operators overloading example

---
+++ LearningCpp/overloading/operators.cpp
@@ -0,0 +1,35 @@
+/***********************************
+ * MMIR01. This is an example of
+ * operator overloading via
+ * friend functions
+************************************/
+
+#include <iostream>
+
+class Car {
+    std::string m_make {};
+    std::string m_model {};
+    int m_value {};
+public:
+    Car (std::string make, std::string model, int value) 
+        : m_make(make), m_model(model), m_value(value) {}
+
+    /* We need to declare here the function, but this is
+    not a member function*/
+    friend int operator+ (const Car& c1, const Car& c2);
+};
+
+//We want to sum to cars and get the combined value of them
+int operator+ (const Car& c1, const Car& c2) {
+    return c1.m_value + c2.m_value;
+}
+
+int main()
+{
+    Car c_one {"BMW", "Series 2", 36000};
+    Car c_two {"Seat", "Ibiza", 19000};
+    
+    std::cout << "Fleet value: " << c_one + c_two << "\n";
+
+    return 0;
+}
