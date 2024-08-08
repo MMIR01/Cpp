31d336bbc6954863cbadbc7b5186437ad4412efa
mmir01 <mmir01@github.com>
2024-08-08 08:45:04 +0100

    Under development. Official cmake tutorial

---
+++ examples/cmake/MathFunctions/CMakeFiles/MathFunctions.dir/MathFunctions.cxx.o.d
@@ -0,0 +1,5 @@
+MathFunctions/CMakeFiles/MathFunctions.dir/MathFunctions.cxx.o: \
+ /home/vboxuser/repos/Cpp/examples/cmake/MathFunctions/MathFunctions.cxx \
+ /usr/include/stdc-predef.h \
+ /home/vboxuser/repos/Cpp/examples/cmake/MathFunctions/MathFunctions.h \
+ /home/vboxuser/repos/Cpp/examples/cmake/MathFunctions/mysqrt.h
